8a4e2b102add277ee5644c52d5c630bf8b59d112
League Student <league@iMac-2.local>
2019-06-13 19:58:03 -0700

    did byte and short printer

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -5,35 +5,43 @@ public class BinaryPrinter {
 	// Don't be afraid to use the methods that are already complete to finish the
 	// others.
 	// Create a main method to test your methods.
-	
+
 	public void printByteBinary(byte b) {
 		String print = "";
-		int output = 0;
-		if(b<0) {
-			print += "1";
-		}else {
-			print += "0";
-		}
-		//find highest value, remove left most value
-		int ctr = 1;
-		while(output < b) {
-			output *= 2;
-		}
-		print += "1";
-		//find combination of values
-		while(output != b) {
-			if(output + output/(2*ctr) < b) {
-				output += output/(2*ctr);
-				print += "1";
-				
-			}
-			print += "0";
-		}
+		print += (b & 128) >> 7;
+		print += (b & 64) >> 6;
+		print += (b & 32) >> 5;
+		print += (b & 16) >> 4;
+		print += (b & 8) >> 3;
+		print += (b & 4) >> 2;
+		print += (b & 2) >> 1;
+		print += (b & 1);
+
 		System.out.println(b + " in binary is " + print);
 	}
 
 	public void printShortBinary(short s) {
+		String print = "";
 
+		print += (s & 32768) >> 16;
+		print += (s & 16384) >> 14;
+		print += (s & 8192) >> 13;
+		print += (s & 4096) >> 12;
+		print += (s & 2048) >> 11;
+		print += (s & 1024) >> 10;
+		print += (s & 512) >> 9;
+		print += (s & 256) >> 8;
+	
+		print += (s & 128) >> 7;
+		print += (s & 64) >> 6;
+		print += (s & 32) >> 5;
+		print += (s & 16) >> 4;
+		print += (s & 8) >> 3;
+		print += (s & 4) >> 2;
+		print += (s & 2) >> 1;
+		print += (s & 1);
+
+		System.out.println(s + " in binary is " + print);
 	}
 
 	public void printIntBinary(int i) {
@@ -43,10 +51,13 @@ public class BinaryPrinter {
 	public void printLongBinary(long l) {
 
 	}
+
 	public static void main(String[] args) {
 		BinaryPrinter bp = new BinaryPrinter();
-		bp.printByteBinary(1);
-		
-		
+		byte by = 2;
+		bp.printByteBinary(by);
+		short sh = 200;
+		bp.printShortBinary(sh);
+
 	}
 }
